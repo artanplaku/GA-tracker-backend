62aa601eaa870cf1552724db49a895236dfb2166
Artan <artanpl03@gmail.com>
2022-11-01 16:08:22 -0400

    change

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'})
+    return jwt.sign({_id}, process.env.SESSION_SECRET_KEY, { expiresIn: '3d'})
 }
 
 // login user
